8e5c2a24711f2ce834022bb4ddfdda44fad40d8b
Alex Bezerra <alexbzrr@outlook.com>
2025-05-02 16:06:33 -0300

    rota

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/rotas/",
+  base: "./",
   plugins: [react()],
   server: {
     open: true,
