9fa7a9f8a7ed539b3079e020862db38c234090c8
Alex Bezerra <alexbzrr@outlook.com>
2025-05-02 14:37:01 -0300

    instalando as dependencias

---
+++ routes/PathsRoutes.tsx
@@ -0,0 +1,35 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import About from "../src/pages/About";
+import Contact from "../src/pages/Contact";
+import HomePage from "../src/pages/HomePage";
+import PagesLayout from "../src/layouts/PageLayout";
+import React from "react";
+const PathsRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<PagesLayout />}>
+          <Route index element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/sobre" element={<About />} />
+          <Route path="/contato" element={<Contact />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <div className="w-full h-screen flex justify-center items-center ">
+              <span
+                style={{ fontSize: "2.5rem" }}
+                className=" text-primary text-9xl font-bold"
+              >
+                404 - Página não encontrada
+              </span>
+            </div>
+          }
+        />
+      </Routes>
+    </div>
+  );
+};
+
+export default PathsRoutes;
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Suspense } from "react";
+import PathsRoutes from "./../routes/PathsRoutes";
+
 function App() {
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Suspense fallback={<div>Carregando...</div>}>
+        <section>
+          <main className=" bg-red-50">
+            <PathsRoutes />
+          </main>
+        </section>
+      </Suspense>
     </>
   );
 }
